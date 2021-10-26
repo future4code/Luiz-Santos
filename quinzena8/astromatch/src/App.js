044ce9caddbed3a30f172aa834217c2987a8b1c7
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-10-26 14:44:31 -0300

    commit inicial

---
+++ quinzena8/astromatch/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import GlobalStyle from './styles/globalStyles';
+
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <p>AstroMatch</p>
+    </Fragment>
+  );
+}
+
+export default App;
