fbf0835d29adc6f2f96838f10908fd9c3bb97d67
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-10-29 00:10:13 -0300

    ajustes finais

--- quinzena8/astromatch/src/components/Content/styles.js
+++ quinzena8/astromatch/src/components/Content/styles.js
@@ -5,11 +5,11 @@ export const MainContainer = styled.main`
   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
   border-radius: 0.5rem;
 
-  width: 100%;
-  max-width: 45rem;
+  /* width: 100%; */
+  width: 45rem;
   height: 90%;
 
   @media (max-width: 500px) {
-    max-width: 30rem;
+    width: 30rem;
   }
 `;
